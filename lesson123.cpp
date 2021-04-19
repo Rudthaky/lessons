42ccd1e51e32204cfd0c252aa51f066792560d78
Petukhov Iliya <Rudthaky@gmail.com>
2021-04-19 23:58:00 +0300

    Custom exception class

---
+++ lesson123.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+/**
+ * создание собственных исключений
+ */
+
+class MyException : public logic_error
+{
+    public:
+        MyException(char* msg, int dataState) : logic_error(msg)
+        {
+            this->dataState = dataState;
+        }
+        int GetDataState() { return dataState; }
+
+    private:
+        int dataState;
+};
+
+void Foo(int value)
+{
+    if (value < 0)
+    {
+        throw logic_error("Namber < 0 !!!"); //NOTE кидаем исключение
+    }
+
+    if (value == 1)
+    {
+        throw MyException("Exception namber", value);
+    }
+    cout << "variable = " << value << endl;
+}
+
+int main()
+{
+    try
+    {
+        Foo(1); //NOTE если вункция не выполняет условие if поподаем в иключение
+    }
+    catch (MyException &ex) //NOTE обрабатываем исключение, тип throw и ex должны совпадать
+    {
+        cout << "We catch " << ex.what() << endl;
+        cout << "Value is = " << ex.GetDataState() << endl;
+    }
+
+    return 0;
+}
